863889111cb9762fe21856224ef1731ee4f4c594
Nicole Valletta <nicolelouisev@gmail.com>
2025-04-08 00:58:55 -0300

    feat: estrutura inicial do projeto com Spring Boot

---
+++ src/main/java/com/example/projetoloja/ProjetolojaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolojaApplication.class, args);
+	}
+
+}
