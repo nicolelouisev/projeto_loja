5943cba18983ac9ed6e3fbb5d3099238c48cdde0
Nicole Valletta <nicolelouisev@gmail.com>
2025-04-08 14:36:28 -0300

    feat: adiciona métodos customizados em ProdutoRepository

--- src/main/java/com/example/projetoloja/repository/ProdutoRepository.java
+++ src/main/java/com/example/projetoloja/repository/ProdutoRepository.java
@@ -1,8 +1,25 @@
 package com.example.projetoloja.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.projetoloja.model.Categoria;
 import com.example.projetoloja.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	boolean existsByNomeAndCategoria(String nome, Categoria categoria);
+	
+	@Query("SELECT p FROM Produto p " +
+	           "WHERE LOWER(p.nome) LIKE LOWER(CONCAT('%', :nome, '%')) " +
+	           "AND LOWER(p.categoria.nome) LIKE LOWER(CONCAT('%', :categoriaNome, '%'))")
+	    List<Produto> buscarPorNomeECategoria(@Param("nome") String nome,
+	                                          @Param("categoriaNome") String categoriaNome);
+	
+	@Query("SELECT p.categoria.nome, SUM(p.quantidadeEstoque) " +
+		       "FROM Produto p GROUP BY p.categoria.nome")
+		List<Object[]> quantidadeEstoquePorCategoria();
 }
