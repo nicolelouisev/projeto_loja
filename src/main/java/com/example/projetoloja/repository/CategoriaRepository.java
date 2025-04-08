c0557902ed2abcbc8368d2ab308e51733d8564bc
Nicole Valletta <nicolelouisev@gmail.com>
2025-04-08 20:35:54 -0300

    chore: ajuste arquivos projeto

--- src/main/java/com/example/projetoloja/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projetoloja.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.projetoloja.model.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    boolean existsByNome(String nome);
-}
